94fa7d97b39b4096aae8827d7a145f5073cedb8d
Biel <biel.nieto@mataro.epiaedu.cat>
2024-10-14 20:48:01 +0200

    sol netlify

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // EXERCISE 1: Selecting Elements
 // Select the element with the ID 'mainTitle' and log it to the console.
-let mainTitleElement = document.getElementById('mainTitle');
+let mainTitleElement = window.document.getElementById('mainTitle');
 
 console.log(mainTitleElement);
 
 // EXERCISE 2: Changing HTML Content
 // Select all paragraph elements and change the content of the second paragraph to "This paragraph has been changed."
-let paragraphElement = document.getElementsByTagName('p');
+let paragraphElement = window.document.getElementsByTagName('p');
 
 paragraphElement[1].innerHTML = "This paragraph has been changed.";
 
 // Exercise 3: Changing CSS Styles
 // Select the first list item and change its text color to red
-let firstListElement = document.querySelector('li')
+let firstListElement = window.document.querySelector('li')
 firstListElement.style.color = 'red';
 
 // Exercise 4: Adding/Removing Classes
@@ -24,11 +24,12 @@ setTimeout(() => {
     paragraphElement[0].classList.remove('highlight');
 }, 3000);
 
+
 // Exercise 5: Creating and Appending Elements
 // Create a new list item with the text "Item 4" and append it to the existing list
-let listElement = document.querySelector('ul');
+let listElement = window.document.querySelector('ul');
 
-let newListElement = document.createElement('li');
+let newListElement = window.document.createElement('li');
 
 newListElement.textContent = 'Item 4';
 
@@ -40,7 +41,7 @@ listElement.removeChild(listElement.lastElementChild);
 
 // Exercise 7: Handling Events
 // Add a click event listener to the button that displays an alert saying "Button clicked!"
-let buttonElement = document.getElementById('actionButton');
+let buttonElement = window.document.getElementById('actionButton');
 
 buttonElement.addEventListener('click', () => {
     alert('Button Clicked!');
@@ -48,8 +49,8 @@ buttonElement.addEventListener('click', () => {
 
 // Exercise 8: Working with Input
 // Add an input event listener to the input field that displays the current input value in the result div
-let inputElement = document.getElementById('inputField');
-let resultElement = document.getElementById('result');
+let inputElement = window.document.getElementById('inputField');
+let resultElement = window.document.getElementById('result');
 
 inputElement.addEventListener('input', () => {
     resultElement.textContent = inputElement.value;
@@ -57,7 +58,7 @@ inputElement.addEventListener('input', () => {
 
 // Exercise 9: Toggling Visibility
 // Add a click event listener to the button that toggles the visibility of the content div
-let contentElement = document.getElementById('content');
+let contentElement = window.document.getElementById('content');
 
 buttonElement.addEventListener('click', () => {
     if (contentElement.style.display == 'none') {
